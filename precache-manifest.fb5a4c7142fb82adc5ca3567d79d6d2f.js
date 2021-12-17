9727b3a3a5d4fe437718e393ffaa1d8798c73ec3
Gagan <bajajsons7479@gmail.com>
2021-12-17 10:08:54 +0530

    'changes'

---
+++ precache-manifest.fb5a4c7142fb82adc5ca3567d79d6d2f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f63aa58cb5292e6fad23",
+    "url": "/sharedspirits/css/app.b41927a4.css"
+  },
+  {
+    "revision": "15d6a990947f56fa9ec699149d4e3ef7",
+    "url": "/sharedspirits/images/wine.png"
+  },
+  {
+    "revision": "048d49ff3d50da154c0b5b0510b1378f",
+    "url": "/sharedspirits/img/background.048d49ff.png"
+  },
+  {
+    "revision": "8055e68a4402045c088af35273d30ef1",
+    "url": "/sharedspirits/index.html"
+  },
+  {
+    "revision": "5c8e0bc27fbe55c898a0a1ba73e75d6f",
+    "url": "/sharedspirits/index.js"
+  },
+  {
+    "revision": "f63aa58cb5292e6fad23",
+    "url": "/sharedspirits/js/app.a79dab0a.js"
+  },
+  {
+    "revision": "6ac068463b74e96a0646",
+    "url": "/sharedspirits/js/chunk-vendors.7e9f2910.js"
+  },
+  {
+    "revision": "3f7e28310ac99663db96dd831a1f3f1f",
+    "url": "/sharedspirits/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sharedspirits/robots.txt"
+  },
+  {
+    "revision": "704f67559212033cfa2813d6cb4bbe49",
+    "url": "/sharedspirits/sw.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sharedspirits/precache-manifest.fb5a4c7142fb82adc5ca3567d79d6d2f.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "fireblogs"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
